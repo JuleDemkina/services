63368dd3ca39a8122e781c0c5390e889b36c020d
Julia Demkina <demkina.yulia@mail.ru>
2024-02-03 14:58:21 +0300

    aa

---
+++ maketmaket/src/index.js
@@ -0,0 +1,11 @@
+import "@babel/polyfill";
+import './index.html';
+import './index.scss';
+
+import './js/read-more';
+import './js/aside';
+import './js/feedback';
+import './js/section-type';
+import './js/swiper';
+import './js/more-brends';
+import './js/callback';
